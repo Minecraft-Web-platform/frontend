58e5f138c6a21ea78e09852805c5dc5611a0a062
oleksandrshtonda <oleksandrshtonda@gmail.com>
2025-09-14 12:51:14 +0200

    implemented login page

--- src/modules/auth/pages/login.page.tsx
+++ src/modules/auth/pages/login.page.tsx
@@ -1,7 +1,58 @@
-import { FC } from "react";
+import { FC, FormEvent, useState } from "react";
+import "./login.page.scss";
+import Input from "../../../shared/ui/input/input.component";
+import { Link, useNavigate } from "react-router-dom";
+import Button from "../../../shared/ui/button/button.component";
+import { authService } from "../services/auth.service";
 
 const LoginPage: FC = () => {
-  return <main className="login-page"></main>;
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const navigate = useNavigate();
+
+  const onSubmitHandler = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const body = {
+      username,
+      password,
+    };
+
+    authService.login(body);
+  };
+
+  return (
+    <main className="login-page">
+      <form className="login-form" onSubmit={(e) => onSubmitHandler(e)}>
+        <h1>Вход</h1>
+
+        <Input
+          value={username}
+          setValue={setUsername}
+          placeholder=""
+          label="Никнейм"
+        />
+
+        <Input
+          value={password}
+          setValue={setPassword}
+          placeholder=""
+          type="password"
+          label="Пароль"
+        />
+
+        <div className="buttons">
+          <Button callback={() => {}}>Войти</Button>
+
+          <Button callback={() => navigate("/registration")} secondary={true}>
+            Еще нет аккаунта?
+          </Button>
+        </div>
+
+        <Link to={"/"}>Забыли пароль?</Link>
+      </form>
+    </main>
+  );
 };
 
 export default LoginPage;
