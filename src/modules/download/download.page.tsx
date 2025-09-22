b3aa3220ee215c4667b80acc046b8aacac56e766
oleksandrshtonda <oleksandrshtonda@gmail.com>
2025-09-22 23:49:21 +0200

    did launcher downloads

--- src/modules/download/download.page.tsx
+++ src/modules/download/download.page.tsx
@@ -1,10 +1,39 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import "./download.page.scss";
 
 import ModBuilder from "./components/mod-builder.component";
 import Sidebar from "../../shared/ui/sidebar/sidebar.component";
 
+type LauncherMeta = {
+  filename: string;
+  size: number;
+  sizeMB: number;
+  version: string;
+  url: string;
+};
+
+type LaunchersResponse = Record<string, LauncherMeta>;
+
+const osList: { key: string; label: string; icon: string }[] = [
+  { key: "windows", label: "Windows", icon: "/svg/windows.svg" },
+  { key: "mac", label: "macOS", icon: "/svg/macos.svg" },
+  { key: "ubuntu", label: "Ubuntu", icon: "/svg/linux-ubuntu.svg" },
+];
+
 const DownloadPage: FC = () => {
+  const [launchers, setLaunchers] = useState<LaunchersResponse | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/launchers/meta")
+      .then((res) => res.json())
+      .then((data: LaunchersResponse) => {
+        setLaunchers(data);
+        setLoading(false);
+      })
+      .catch(() => setLoading(false));
+  }, []);
+
   return (
     <div className="download-page">
       <Sidebar />
@@ -13,30 +42,29 @@ const DownloadPage: FC = () => {
         <section className="download-launcher">
           <h1>Скачай лаунчер</h1>
 
+          {loading && <p>Загрузка...</p>}
+
           <div className="download-launcher__files">
-            <div className="download-launcher__os">
-              <span>Windows</span>
+            {osList.map((os) => {
+              const meta = launchers?.[os.key];
 
-              <img src="/svg/windows.svg" />
-            </div>
+              if (!meta) {
+                return <p className="unavailable">Недоступно</p>;
+              }
 
-            <div className="download-launcher__os">
-              <span>macOS</span>
-
-              <img src="/svg/macos.svg" />
-            </div>
-
-            <div className="download-launcher__os">
-              <span>Ubuntu</span>
-
-              <img src="/svg/linux-ubuntu.svg" />
-            </div>
-
-            <div className="download-launcher__os">
-              <span>Jar</span>
-
-              <img src="/svg/java.svg" />
-            </div>
+              return (
+                <a
+                  className="download-launcher__os"
+                  key={os.key}
+                  href={"https://api.khroniki-kraya.com" + meta.url}
+                  download={meta.filename}
+                >
+                  <span>{os.label}</span>
+                  <img src={os.icon} alt={os.label} />
+                  <span>{meta.sizeMB}MB</span>
+                </a>
+              );
+            })}
           </div>
         </section>
 
