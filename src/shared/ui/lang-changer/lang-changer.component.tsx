6b7cfc358219924116cb0f068dda827034da65bb
oleksandrshtonda <oleksandrshtonda@gmail.com>
2026-04-01 17:50:42 +0200

    removed redunant event passing

--- src/shared/ui/lang-changer/lang-changer.component.tsx
+++ src/shared/ui/lang-changer/lang-changer.component.tsx
@@ -2,9 +2,7 @@ import { Dispatch, SetStateAction, useState } from "react";
 import i18n from "../../../i18n/i18n";
 import './lang-changer.component.scss';
 
-function changeLanguage(event: React.MouseEvent<HTMLButtonElement, MouseEvent>, language: string, setter: Dispatch<SetStateAction<string>>) {
-  event.preventDefault();
-
+function changeLanguage(language: string, setter: Dispatch<SetStateAction<string>>) {
   i18n.changeLanguage(language);
   setter(language);
   
@@ -15,10 +13,10 @@ export const LangChanger = () => {
 
   return (
     <div className="lang-changer">
-      <button className={`btn lang-changer__btn${lang == 'ru' ? ' btn--chosen' : 'ru'}`} onClick={e => changeLanguage(e, 'ru', setLang)}>RU</button>
-      <button className={`btn lang-changer__btn${lang == 'ua' ? ' btn--chosen' : 'ua'}`} onClick={e => changeLanguage(e, 'ua', setLang)}>UA</button>
-      <button className={`btn lang-changer__btn${lang == 'pl' ? ' btn--chosen' : 'pl'}`} onClick={e => changeLanguage(e, 'pl', setLang)}>PL</button>
-      <button className={`btn lang-changer__btn${lang == 'en' ? ' btn--chosen' : 'en'}`} onClick={e => changeLanguage(e, 'en', setLang)}>EN</button>
+      <button type="button" className={`btn lang-changer__btn${lang == 'ru' ? ' btn--chosen' : 'ru'}`} onClick={() => changeLanguage('ru', setLang)}>RU</button>
+      <button type="button" className={`btn lang-changer__btn${lang == 'ua' ? ' btn--chosen' : 'ua'}`} onClick={() => changeLanguage('ua', setLang)}>UA</button>
+      <button type="button" className={`btn lang-changer__btn${lang == 'pl' ? ' btn--chosen' : 'pl'}`} onClick={() => changeLanguage('pl', setLang)}>PL</button>
+      <button type="button" className={`btn lang-changer__btn${lang == 'en' ? ' btn--chosen' : 'en'}`} onClick={() => changeLanguage('en', setLang)}>EN</button>
     </div>
   )
 }
