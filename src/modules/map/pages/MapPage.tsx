bf98a365b5f49651ca87602444c0c1d47c4a8996
oleksandrshtonda <oleksandrshtonda@gmail.com>
2026-08-25 14:53:11 +0200

    refactor(map): remove unused map adapters and models

--- src/modules/map/pages/MapPage.tsx
+++ src/modules/map/pages/MapPage.tsx
@@ -1,45 +1,11 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef } from 'react';
 import './MapPage.scss';
-import { ITerritory, IMapProvider } from '../models/map.types';
-import { DynmapAdapter } from '../adapters/dynmap.adapter';
-import { BlueMapAdapter } from '../adapters/bluemap.adapter';
 import Sidebar from '../../../shared/ui/sidebar/sidebar.component';
 
 export const MapPage: React.FC = () => {
   const iframeRef = useRef<HTMLIFrameElement>(null);
-  const [territories, setTerritories] = useState<ITerritory[]>([]);
-  const [adapter, setAdapter] = useState<IMapProvider | null>(null);
 
   const mapUrl = '/bluemap/'; 
-
-  useEffect(() => {
-    const fetchTerritories = async () => {
-      try {
-        const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000';
-        const res = await fetch(`${BACKEND_URL}/territories`);
-        if (res.ok) {
-          const data = await res.json();
-          setTerritories(data);
-        }
-      } catch (e) {
-        console.error('Failed to fetch territories', e);
-      }
-    };
-    fetchTerritories();
-  }, []);
-
-  useEffect(() => {
-    setAdapter(new BlueMapAdapter());
-  }, []);
-
-  const handleIframeLoad = () => {
-    if (iframeRef.current && adapter) {
-      adapter.init(iframeRef.current);
-      setTimeout(() => {
-        adapter.drawTerritories(territories);
-      }, 2000);
-    }
-  };
 
   return (
     <div className="page map-root">
@@ -61,7 +27,6 @@ export const MapPage: React.FC = () => {
               src={mapUrl}
               title="Interactive Map"
               className="map-iframe"
-              onLoad={handleIframeLoad}
               allowFullScreen
             ></iframe>
           </div>
